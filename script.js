1dd12de268c7d4a540c9bc2b70a50bf76f7f7403
elvismourab <152972333+elvismourab@users.noreply.github.com>
2024-03-11 21:51:01 -0300

    Add initial square class + div

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ const body = document.querySelector("body");
 const container = document.createElement("div");
 container.setAttribute("class", "container");
 body.appendChild(container);
+
+const square = document.createElement("div");
+square.setAttribute("class", "square");
+container.appendChild(square);
