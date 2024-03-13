80d17d9e6f2c656ee629c57daf5217fdab809556
elvismourab <152972333+elvismourab@users.noreply.github.com>
2024-03-13 06:38:19 -0300

    Add changes to style and script*
    - Change body and container to flex.
    
    - Add js iteration to fill container with divs
    The JavaScript code calculates how many child divs can fit into the parent div based on the static width and height of the child divs.
    It then creates and appends the child divs to the parent div until it's filled.

--- script.js
+++ script.js
@@ -4,12 +4,26 @@ const container = document.createElement("div");
 container.setAttribute("class", "container");
 body.appendChild(container);
 
-const square = document.createElement("div");
+/* const square = document.createElement("div");
 square.setAttribute("class", "square");
 container.appendChild(square);
 
-/* for (let i = 0; i <= 1000; i++) {
+for (let i = 0; i <= 4276; i++) {
     const square = document.createElement("div");
     square.setAttribute("class", "square");
     container.appendChild(square);
 } */
+
+const childHeight = 16;
+const childWidth = 16;
+
+let numChildHeight = Math.floor(container.offsetHeight / childHeight); console.log(numChildHeight);
+let numChildWidth = Math.floor(container.offsetWidth / childWidth); console.log(numChildWidth);
+
+for (let i = 0; i <= numChildHeight; i++) {
+    for (let j = 0; j <= numChildWidth; j++) {
+        const square = document.createElement("div");
+        square.setAttribute("class", "square");
+        container.appendChild(square);
+    }
+}
