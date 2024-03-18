89fcb30b5d6195b23fdaa5da6d4d8b0d0c5816ca
elvismourab <152972333+elvismourab@users.noreply.github.com>
2024-03-18 11:50:28 -0300

    Add mouse move event - paint

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+// create parent div
 const body = document.querySelector("body");
 
 const container = document.createElement("div");
 container.setAttribute("id", "container");
 body.appendChild(container);
 
+// 2. 16x16 grid
 for (let i = 0; i < 16; i ++) {
     for (let j = 0; j < 16; j++) {
         const square = document.createElement("div");
@@ -17,16 +19,10 @@ for (let i = 0; i < 16; i ++) {
     }
 }
 
-//
-/* let count = 0;
-
-do {
-    const square = document.createElement("div");
-    square.setAttribute("class", "square");
-    
-/*     square.style.height = (container.offsetHeight / 16) + "px";
-    square.style.width = (container.offsetWidth / 16) + "px";
-    
-    container.appendChild(square);
-    count++;
-} while (count <= 256); // 16 * 16 */
+// 3. Mouse move event - paint
+const mouseMove = document.getElementsByClassName("square");
+for (i = 0; i < mouseMove.length; i++) {
+    mouseMove[i].addEventListener('mouseover', function() {
+        this.style.backgroundColor = "black";
+    })
+}
