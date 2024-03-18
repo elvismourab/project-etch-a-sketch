88fcb582a7b561fbccfcc4d854cc0b3c3e5c751f
elvismourab <152972333+elvismourab@users.noreply.github.com>
2024-03-18 10:42:17 -0300

    ! Solve the problem of overflow squares

--- script.js
+++ script.js
@@ -4,24 +4,29 @@ const container = document.createElement("div");
 container.setAttribute("id", "container");
 body.appendChild(container);
 
-/* const square = document.createElement("div");
-square.setAttribute("id", "square");
-
-square.style.height = (container.offsetHeight / 16) + "px";
-square.style.width = (container.offsetWidth / 16) + "px";
-
-container.appendChild(square); */
+for (let i = 0; i < 16; i ++) {
+    for (let j = 0; j < 16; j++) {
+        const square = document.createElement("div");
+        square.setAttribute("class", "square");
+        square.style.boxSizing = "border-box";
+        
+        square.style.height = (container.offsetHeight / 16) + "px";
+        square.style.width = (container.offsetWidth / 16) + "px";
+        
+        container.appendChild(square);
+    }
+}
 
 //
-let count = 0;
+/* let count = 0;
 
 do {
     const square = document.createElement("div");
-    square.setAttribute("id", "square");
+    square.setAttribute("class", "square");
     
-    square.style.height = (container.offsetHeight / 16) + "px";
+/*     square.style.height = (container.offsetHeight / 16) + "px";
     square.style.width = (container.offsetWidth / 16) + "px";
     
     container.appendChild(square);
     count++;
-} while (count < 16); // 16 * 16
+} while (count <= 256); // 16 * 16 */
