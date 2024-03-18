065646e18eb30409ff682466851a28026f9fa589
elvismourab <152972333+elvismourab@users.noreply.github.com>
2024-03-18 14:31:16 -0300

    Encapsulate forms in their respective functions
    - Create gridStart function for inner divs.
    - Create paint function for mouse event.

--- script.js
+++ script.js
@@ -6,38 +6,48 @@ container.setAttribute("id", "container");
 body.appendChild(container);
 
 // 2. 16x16 grid
-for (let i = 0; i < 16; i ++) {
-    for (let j = 0; j < 16; j++) {
-        const square = document.createElement("div");
-        square.setAttribute("class", "square");
-        square.style.boxSizing = "border-box";
-        
-        square.style.height = (container.offsetHeight / 16) + "px";
-        square.style.width = (container.offsetWidth / 16) + "px";
-        
-        container.appendChild(square);
+function gridStart(grid = 16) {
+    for (let i = 0; i < grid; i ++) {
+        for (let j = 0; j < grid; j++) {
+            const square = document.createElement("div");
+            square.setAttribute("class", "square");
+            square.style.boxSizing = "border-box";
+            
+            square.style.height = (container.offsetHeight / grid) + "px";
+            square.style.width = (container.offsetWidth / grid) + "px";
+            
+            container.appendChild(square);
+        }
     }
 }
 
 // 3. Mouse move event - paint
-const mouseMove = document.getElementsByClassName("square");
-for (i = 0; i < mouseMove.length; i++) {
-    mouseMove[i].addEventListener('mouseover', function() {
-        this.style.backgroundColor = "black";
-    })
+function paint() {
+    const mouseMove = document.getElementsByClassName("square");
+    for (i = 0; i < mouseMove.length; i++) {
+        mouseMove[i].addEventListener('mouseover', function() {
+            this.style.backgroundColor = "black";
+        })
+    }
 }
 
 // 4 - button / div size
 const btn = document.createElement("button");
 btn.textContent = "NEW GRID";
-//btn.setAttribute("id", "newGrid");
-
 btn.addEventListener("click", function() {
-    let squares = 16;
+    let squareSize = 16;
     do {
-        squares = prompt("Define the number of squares per side for the new grid (<= 100): ");
-        if (squares > 100) alert("The value should be <= 100");
-    } while (squares > 100);
+        squareSize = prompt("Define the number of squareSize per side for the new grid (<= 100): ");
+        if (squareSize > 100) alert("The value should be <= 100");
+    } while (squareSize > 100);
+
+    container.innerHTML = "";
+    gridStart(squareSize);
+    paint();
 })
 
 body.prepend(btn);
+
+// START
+gridStart();
+paint();
